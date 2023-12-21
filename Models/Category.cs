c4395b2d14fe2fbf7db636be805fa53104bbbaf3
Momen safi <Safi.moumen90@gmail.com>
2023-12-21 10:07:54 +0300

    edit relations

--- Models/Category.cs
+++ Models/Category.cs
@@ -11,5 +11,6 @@ namespace MVC2.Models
         public string CategoryName { get; set; }
         [Required]
         public string CategoryImage { get; set; }
+        public ICollection<Category> Categories { get; set;}
     }
 }
